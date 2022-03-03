432dc57e400a5a4f99279f0950fc56d323fcfe28
Minsu Lee <amond@amond.net>
2022-03-03 18:31:42 +0900

    feat: dart 2.15

--- dart/lib/vercel_dart_runtime.dart
+++ dart/lib/vercel_dart_runtime.dart
@@ -1,14 +1,16 @@
 import 'package:aws_lambda_runtime/aws_lambda_runtime.dart' as aws;
 import 'package:shelf/shelf.dart' as shelf;
 
-import 'package:vercel_dart/src/models/event.dart';
-import 'package:vercel_dart/src/utils/response_to_object.dart';
+import 'package:vercel_dart_runtime/src/models/event.dart';
+import 'package:vercel_dart_runtime/src/utils/response_to_object.dart';
 
 void handle(shelf.Handler handler, {String name = 'bootstrap'}) {
   // ignore: prefer_function_declarations_over_variables
   final aws.Handler<Event> _handler = (context, event) async {
     final res = await handler(event.body);
-    return aws.InvocationResult(context.requestId, await responseToObject(res));
+
+    return aws.InvocationResult(
+        context.requestId!, await responseToObject(res));
   };
 
   aws.Runtime()
