f7a71bb52ba62723e280cc90830bcbd064510283
Minsu Lee <amond@amond.net>
2022-05-23 12:12:15 +0900

    feat: dart 2.17

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,10 @@
 export const DEFAULT_DART_CHANNEL = 'stable'
-export const DEFAULT_DART_VERSION = '2.15.1'
-export const SDK_VERSION = '">=2.15.0 <3.0.0"'
+export const DEFAULT_DART_VERSION = '2.17.0'
+export const SDK_VERSION = '">=2.12.0 <3.0.0"'
 export const RUNTIME_PKG = {
   git: {
-    url: 'https://github.com/amondnet/vercel-dart',
+    url: 'https://github.com/amondnet/vercel-dart-runtime',
     path: 'dart',
+    ref: '31a1b32ecd3534ac7463843faac1d9aac960ebca'
   },
 }
